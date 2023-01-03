effd1d8e29eba8b98287a1ef07e1ee7fa6c065bb
sebastiap <sebastiap@gmail.com>
2023-01-02 22:21:18 -0300

    experimental added to nextconfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains: ['images.unsplash.com','hydeparkwinterwonderland.com','wembleypark.com']
+  },
+  experimental: { 
+    nftTracing: true 
   }
 }
 
